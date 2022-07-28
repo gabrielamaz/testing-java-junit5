89480ff4a3d45675c3155ec730cebb1210a1befd
Gabriela Azevedo <gabrielaazevedo@frwk.com.br>
2022-07-28 11:46:54 -0300

    Implementando método default p/ testes em interface

--- src/test/java/guru/springframework/ControllersTest.java
+++ src/test/java/guru/springframework/ControllersTest.java
@@ -1,7 +1,15 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("Controllers")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllersTest {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Lets do something here");
+    }
 }
