bc3e015a4f731f55c2ad09f97e497704d0ba08c3
Gabriela Azevedo <gabrielaazevedo@frwk.com.br>
2022-10-10 12:19:00 -0300

    Exemplos de mocks inline, annotation e extension. Exemplos de testes unitários em serviços com mocks de repositórios

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceTestIT.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.junitextensions.TimingExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(TimingExtension.class)
+class PetTypeSDJpaServiceTestIT {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
